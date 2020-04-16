acd32e713da715443912146fbbd61179a4cc3502
zibingala <47749140+zibingala@users.noreply.github.com>
2020-04-16 14:46:36 +0200

    updated start

--- social_distance/sketch.js
+++ social_distance/sketch.js
@@ -84,8 +84,6 @@ switch(mode){
         text('SEND HOME W/ CLICK + MOVE w/ WASD', width/2 , height/2 + 3*textSIZE);
                 fill(random(255),random(255),random(255),50);
         text('NOW STRT WITH CLICK', width/2 , height/2 + 5*textSIZE);
-                        fill(random(255),random(255),random(255),50);
-        text('use CHROME or BRAVE to HAVE FUN!', width/2 , height/2 + 6*textSIZE);
         
         
         break;
